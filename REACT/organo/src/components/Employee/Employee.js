9a58d51ff8b0dcfdee0ed09969c661bab3ccde04
MakenRosa <maken.cristhian@gmail.com>
2023-06-30 11:35:10 -0300

    fix(react): fixed prop job not passed to component

--- REACT/organo/src/components/Employee/Employee.js
+++ REACT/organo/src/components/Employee/Employee.js
@@ -1,14 +1,14 @@
 import './Employee.css'
 
-export const Employee = ({name, job, image, team, primaryColor, ...props}) => {
+export const Employee = ({name, job, image, primaryColor, ...props}) => {
     return (
-        <div className='employee'>
+        <div className='employee' {...props}>
             <div className='header-employee' style={{backgroundColor: primaryColor}}>
                 <img src={image} alt={name} />
             </div>
             <div className='footer-employee'>
                 <h4>{name}</h4>
-                <h5>Estagiário</h5>
+                <h5>{job}</h5>
             </div>
         </div>
     )
