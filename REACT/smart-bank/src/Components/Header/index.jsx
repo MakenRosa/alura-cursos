2686b27f1d38e23cc54d4e6b65d76b4391948a56
MakenRosa <maken.cristhian@gmail.com>
2023-06-30 13:24:47 -0300

    refactor(react): added StyledHeader

--- REACT/smart-bank/src/Components/Header/index.jsx
+++ REACT/smart-bank/src/Components/Header/index.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 import bank_logo from "../../assets/images/bank_logo.svg";
+
+import { primaryColor } from "../UI/variables";
+
+const StyledHeader = styled.nav`
+  background-color: ${primaryColor};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`
+
+const Logo = styled.img`
+  height: 50px;
+  width: 50px;
+`
 
 const Header = () => {
   return (
-    <div className="header">
-      <img className="logo-image" src={bank_logo} alt="Logo Smart Bank" />
+    <StyledHeader>
+      <Logo src={bank_logo} alt="Logo Smart Bank" />
       <div>
         <a className="btn-secondary" href="https://google.com">
           Help
@@ -13,7 +30,7 @@ const Header = () => {
           Quit
         </a>
       </div>
-    </div>
+    </StyledHeader>
   );
 };
 
