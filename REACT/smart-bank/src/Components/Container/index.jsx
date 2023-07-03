6eb55f481306a43456efe267023c5c294415687a
MakenRosa <maken.cristhian@gmail.com>
2023-07-03 11:51:15 -0300

    feat(react): added bank statement and item(s) components

--- REACT/smart-bank/src/Components/Container/index.jsx
+++ REACT/smart-bank/src/Components/Container/index.jsx
@@ -3,9 +3,10 @@ import styled from "styled-components";
 
 import Title from "../Title";
 import Account from "../Account";
+import Statement from "../Statement";
 
 const StyledContainer = styled.div`
-  background-color: #f1f1f1;
+  background-color: ${({ theme }) => theme.body};
   min-height: 90vh;
   padding: 0px 15vw;
 `
@@ -14,6 +15,7 @@ const Content = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  color: ${({ theme }) => theme.text};
   
   @media (max-width: 800px) {
     flex-direction: column;
@@ -26,8 +28,7 @@ const Container = () => {
       <Title>Hello John Doe!</Title>
       <Content>
         <Account />
-        <Account />
-
+        <Statement />
       </Content>
     </StyledContainer>
   );
