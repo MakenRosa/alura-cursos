b14ae2a6710e0fa514b2207c5348930f64e4be21
makenrosa <maken.cristhian@gmail.com>
2023-07-05 19:00:29 -0300

    feature(core/html5-css3/react/typescript): Restructured repository to use submodules as projects

--- REACT/organo/src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import './Button.css'
-
-export const Button = ({ children, ...props }) => {
-    return (
-        <button className="button" {...props}>
-            {children}
-        </button>
-    )
-    }
